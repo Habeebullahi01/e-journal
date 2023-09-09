31a8324fa21e534435dee8ac3c014e97a15c0bbb
Lawal Habeebullahi <lawalhabeebullahi008@gmail.com>
2023-09-09 21:39:19 +0100

    Feature: View Handler

--- handlers.go
+++ handlers.go
@@ -7,13 +7,34 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
-// func loadAllEntries() {
-// 	// checks /data for all the files it contains
-// 	// Reads the data within
-// 	// Returns a slice containing each as *Entry
-// }
+// Checks /data for all the files it contains
+// Reads the data within
+// Returns a slice containing each as *Entry
+func loadAllEntries(reqPath string) ([]Entry, error) {
+
+	var e []Entry
+	filepath.WalkDir("data", func(path string, d fs.DirEntry, err error) error {
+		if !d.IsDir() {
+			// Open the file and read the Entrydate
+			// file, err := os.Open(path)
+			// if err != nil {
+			// 	return err
+			// }
+			// defer file.Close()
+			var content *Entry
+			b, _ := os.ReadFile(path)
+			if json.Valid(b) {
+				json.Unmarshal(b, &content)
+				e = append(e, *content)
+			}
+		}
+		return nil
+	})
+	return e, nil
+}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	// Ensure the existence of /data
@@ -24,38 +45,24 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	//Get names of all available entry files
 
 	var availableEntries []Entry
-	filepath.WalkDir("data", func(path string, d fs.DirEntry, err error) error {
-		if !d.IsDir() {
-			// Open the file and read the Entrydate
-			file, err := os.Open(path)
-			if err != nil {
-				return err
-			}
-			defer file.Close()
-			var content *Entry
-			b, _ := os.ReadFile(path)
-			if json.Valid(b) {
-				json.Unmarshal(b, &content)
-				availableEntries = append(availableEntries, *content)
-			}
-		}
-		return nil
-	})
+	availableEntries, _ = loadAllEntries("")
 	err := templates.ExecuteTemplate(w, "home.html", availableEntries)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	// entry, err := load(title)
-	// if err != nil {
-	// 	http.Error(w, err.Error(), http.StatusNoContent)
-	// 	return
-	// }
+func viewHandler(w http.ResponseWriter, r *http.Request, path []string) {
+	// e:= &EntryDate{Year: path[1], Month: path[2],}
+	entry, err := load(path)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
 	// fmt.Fprint(w, entry)
-	// renderTemplate(w, "view", entry)
+	renderTemplate(w, "view", entry)
+	// e, nil:=load(path)
 }
 
 // func editHandler(w http.ResponseWriter, r *http.Request, title string) {
@@ -68,15 +75,25 @@ func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 // 	// redirect to /error/pastdate or /error/futuredate as necessitated by error
 // }
 
-func makeHandler(fn func(w http.ResponseWriter, r *http.Request, title string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func makeHandler(fn func(w http.ResponseWriter, r *http.Request, path []string)) http.HandlerFunc {
 
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-			// log.Fatal("m is empty!")
-			return
-		}
-		fn(w, r, m[2])
+	return func(w http.ResponseWriter, r *http.Request) {
+		d := r.URL.Path
+		ds := strings.Split(d, "/")
+		ds = ds[2:]
+		// if validPath.MatchString(strings.Join(ds, "/")) {
+
+		// 	fn(w, r, ds)
+		// } else {
+		// 	http.Error(w, "Path malformed", http.StatusNotFound)
+		// }
+		// fmt.Println(ds)
+		// m := validPath.FindStringSubmatch(r.URL.Path)
+		// if m == nil {
+		// 	http.NotFound(w, r)
+		// 	// log.Fatal("m is empty!")
+		// 	return
+		fn(w, r, ds)
+		// }
 	}
 }
--- journal.go
+++ journal.go
@@ -1,45 +1,45 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"regexp"
+	"os"
+	"strings"
 )
 
-var validPath = regexp.MustCompile("/(edit|view|save)/([a-zA-Z0-9]+)$")
+// var validPath = regexp.MustCompile("/(edit|view|save)/([0-9]+)/([A-Z][a-zA-Z])/(2[0-9]-[A-Z][a-zA-Z])$")
 
 // var validViewPath = regexp.MustCompile("/view/([0-9])")
 
 var templates = template.Must(template.ParseFiles("templates/view.html", "templates/home.html"))
 
-// func load(entryDate EntryDate) (*Entry, error) {
-// 	// "title" can be a map of day,month and year
-// 	// os.Open("/data/"+title.year+"/"+title.month+"/"+title.day+".json")
-// 	// Read the json file and create Entry instance using the key-value pairs
-// 	file, err := os.Open("/data/" + string(entryDate.Year) + "/" + entryDate.Month + "/" + string(entryDate.Day) + ".txt")
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer file.Close()
-// 	body, err := os.ReadFile(file.Name())
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	// var date EntryDate
-// 	// date.FullDate = time.Now()
-// 	// date.Year, _, date.Day = date.FullDate.EntryDate()
-// 	// date.Month = date.FullDate.Month().String()
-// 	// date.Weekday = date.FullDate.Weekday().String()
-// 	title := string(entryDate.Day) + "-" + entryDate.Weekday
-// 	e := &Entry{Title: title, Date: entryDate, Body: string(body)}
-// 	return e, nil
-// }
+func load(path []string) (*Entry, error) {
+	// "title" can be a map of day,month and year
+	// os.Open("/data/"+title.year+"/"+title.month+"/"+title.day+".json")
+	// Read the json file and create Entry instance using the key-value pairs
+	// file, err := os.Open("data/" + strings.Join(path, "/") + ".json")
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// defer file.Close()
+	var content *Entry
+	b, err := os.ReadFile("data/" + strings.Join(path, "/") + ".json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	if json.Valid(b) {
+		json.Unmarshal(b, &content)
+	}
+	return content, nil
+}
 
-// func renderTemplate(w http.ResponseWriter, name string, data *Entry) {
+func renderTemplate(w http.ResponseWriter, name string, data *Entry) {
 
-// 	templates.ExecuteTemplate(w, name+".html", data)
-// }
+	templates.ExecuteTemplate(w, name+".html", data)
+}
 
 func main() {
 	// File Organization in Golang
